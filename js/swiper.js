db9c1af3df1cecd8784167cd09bbd86c86ff73a3
AdrianRezende <125374440+AdrianRezende@users.noreply.github.com>
2024-05-10 00:17:51 -0300

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.prev',
+      next: '.next'
+    }
+  });
